def86e7ac5eaae94432c6361f948a3bd7ab93530
ThitinatIM-kiyuu <75411172+ThitinatIM-kiyuu@users.noreply.github.com>
2021-07-30 02:50:13 +0700

    commit1.6

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,9 +1,29 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="edit"> Edit </button> 
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore'], //<--- Inject the Global Store
+  methods: {
+    edit() {
+      //Assuming successful API call to register them
+       this.GStore.flashMessage =
+       'Updated ' + this.event.title
+       setTimeout(() => {
+         //After 3 seconds remove it
+         this.GStore.flashMessage = ''
+       }, 3000)
+      //Set the flash message to appear on the next page loaded which says
+      //'You are sucessfully registered for ' +this.event.title
+
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
